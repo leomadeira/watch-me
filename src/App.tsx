12a2d33217ad10ed2c28b9581bfa67b7e98453b8
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-12-10 23:55:31 -0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import { useEffect, useState } from 'react';
-
 import { Button } from './components/Button';
 import { MovieCard } from './components/MovieCard';
-
 // import { SideBar } from './components/SideBar';
 // import { Content } from './components/Content';
-
 import { api } from './services/api';
-
 import './styles/global.scss';
-
 import './styles/sidebar.scss';
 import './styles/content.scss';
 
